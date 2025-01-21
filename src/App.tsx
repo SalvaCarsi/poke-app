cb54ba7bef354404bea693678cc748e396a513d9
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-21 18:49:47 +0100

    use prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ const App = () => {
   return (
     <>
       <h1>Pokemon Library</h1>
-      <div className="card">
-      </div>
+      <div className="card"></div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
-
 }
-
 
 export default App

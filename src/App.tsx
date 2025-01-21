dab662fcf34c74cdcf55d42313ec3ad2c3fdafd0
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-21 18:45:09 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h1>Pokemon Library</h1>
+      <div className="card">
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+
+}
+
+
+export default App

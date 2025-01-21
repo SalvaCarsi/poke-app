934c6cabfee45687667a3a16cb8a5a8828cb5e75
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-21 19:09:20 +0100

    eslint and prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false, // Disable semicolons
+        singleQuote: true, // Use single quotes
+      },
+    ],
+  },
+  ignorePatterns: ['node_module', 'dist', 'public', 'build'],
+}

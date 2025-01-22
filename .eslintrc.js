c1e2df59defc1006ad8fa3956f51bc1ed1956424
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-22 19:16:03 +0100

    disable eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-refresh', '@typescript-eslint'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-  },
-}
+// export default {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:react-hooks/recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:prettier/recommended',
+//   ],
+//   overrides: [],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//   },
+//   plugins: ['react', 'react-refresh', '@typescript-eslint'],
+//   rules: {
+//     'react-refresh/only-export-components': 'warn',
+//     '@typescript-eslint/no-non-null-assertion': 'off',
+//     '@typescript-eslint/no-explicit-any': 'off',
+//     'react/react-in-jsx-scope': 'off',
+//     'react/jsx-uses-react': 'off',
+//   },
+// }

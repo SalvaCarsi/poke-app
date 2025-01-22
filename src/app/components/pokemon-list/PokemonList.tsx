e0a0640034d60ea877ce976eee919e1642a7771a
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-22 20:38:36 +0100

    componetize

--- src/app/components/pokemon-list/PokemonList.tsx
+++ src/app/components/pokemon-list/PokemonList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { fetchAllPokemons, Pokemon } from '@/app/infra/client'
 import { debounce } from '@/utils'
+import { PokemonListResults } from '@/app/components/pokemon-list/PokemonListResults'
 
 import './PokemonList.css'
 
@@ -43,24 +44,7 @@ const PokemonList = () => {
             <input id="search-input" onChange={debounce(performSearch, 400)} />
           </div>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Url</th>
-            </tr>
-          </thead>
-          <tbody>
-            {searchResults.map((pokemon: Pokemon) => {
-              return (
-                <tr key={pokemon.name}>
-                  <td className="table-cell__pokemon-name">{pokemon.name}</td>
-                  <td className="table-cell__pokemon-url">{pokemon.url}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <PokemonListResults searchResults={searchResults} />
       </div>
     </div>
   )
---
+++ src/app/components/pokemon-list/PokemonListResults.tsx
@@ -0,0 +1,30 @@
+import { Pokemon } from '@/app/infra/client'
+
+interface PokemonListResultsProps {
+  searchResults: Pokemon[]
+}
+
+const PokemonListResults = ({ searchResults }: PokemonListResultsProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Url</th>
+        </tr>
+      </thead>
+      <tbody>
+        {searchResults.map((pokemon: Pokemon) => {
+          return (
+            <tr key={pokemon.name}>
+              <td className="table-cell__pokemon-name">{pokemon.name}</td>
+              <td className="table-cell__pokemon-url">{pokemon.url}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export { PokemonListResults }

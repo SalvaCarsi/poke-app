85699a8d24d7337e7762d79e513fd6edcdcd8c91
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-22 19:30:17 +0100

    fetch pokemons

--- src/app/components/PokemonList.tsx
+++ src/app/components/PokemonList.tsx
@@ -1,4 +1,28 @@
+import { useEffect } from 'react'
+
+interface Pokemon {
+  name: string
+  url: string
+}
+
+const fetchPokemons: () => Promise<Pokemon[]> = async () => {
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0'
+
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return data.results
+}
+
 const PokemonList = () => {
+  const foo = async () => {
+    const response = await fetchPokemons()
+    console.log('>>>>>>>>>>>>>', response)
+  }
+
+  useEffect(() => {
+    foo()
+  })
   return <div>Here goes the list</div>
 }
 

7e81b0f9877a041bac6b8f64da694e41c2ed1989
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-23 19:28:41 +0100

    display detail

--- src/app/infra/client.ts
+++ src/app/infra/client.ts
@@ -3,6 +3,30 @@ export interface Pokemon {
   url: string
 }
 
+export interface PokemonDetail {
+  name: string
+  sprites: string
+  abilities: string[]
+  moves: string[]
+  forms: string
+}
+
+interface AbilityRecord {
+  ability: Ability
+}
+
+interface Ability {
+  name: string
+}
+
+interface MoveRecord {
+  move: Move
+}
+
+interface Move {
+  name: string
+}
+
 export const fetchAllPokemons: () => Promise<Pokemon[]> = async () => {
   const url = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0'
 
@@ -10,4 +34,22 @@ export const fetchAllPokemons: () => Promise<Pokemon[]> = async () => {
   const data = await response.json()
 
   return data.results
+}
+
+export const fetchPokemon = async (id: string) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
+
+  const response = await fetch(url)
+  const detail = await response.json()
+
+  return {
+    name: detail.name,
+    sprites: detail.sprites.back_default,
+    abilities: detail.abilities.map((record: AbilityRecord) => {
+      return record.ability.name
+    }),
+    moves: detail.moves.map((record: MoveRecord) => {
+      return record.move.name
+    }),
+  }
 }
--- src/pages/pokemon-detail/PokemonDetail.tsx
+++ src/pages/pokemon-detail/PokemonDetail.tsx
@@ -1,7 +1,17 @@
 import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { fetchPokemon } from '@/app/infra/client'
 
 const PokemonDetail = () => {
   const { id } = useParams()
+  useEffect(() => {
+    const foo = async () => {
+      const response = await fetchPokemon(id)
+      console.log(response)
+    }
+
+    foo()
+  }, [])
 
   return `holi id ${id}`
 }

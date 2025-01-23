db652711741f4731406f199ce0c18c061a220aa9
Salva Carsi Moreno <salvavcarsi@gmail.com>
2025-01-23 18:47:53 +0100

    detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { PokemonList } from '@/pages/pokemon-list'
+import { PokemonDetail } from '@/pages/pokemon-detail'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PokemonList />} />
+        <Route path="/detail/:id" element={<PokemonDetail />} />
       </Routes>
     </BrowserRouter>
   )
